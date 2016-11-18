a2ffdb75a1522d233bbd5f9c71f3c94431798539
xiaojinzi <347837667@qq.com>
2016-11-18 19:09:18 +0800

    add answer  response of bugDynamics detail

--- src/com/xiaojinzi/code/modular/dynamics/pojo/BugDynamics.java
+++ src/com/xiaojinzi/code/modular/dynamics/pojo/BugDynamics.java
@@ -1,11 +1,25 @@
 package com.xiaojinzi.code.modular.dynamics.pojo;
 
+import com.xiaojinzi.code.modular.comment.pojo.BugComment;
+
 /**
  * Created by xiaojinzi on 2016/10/28.
  * desc:动态,展示在首页
  */
 public class BugDynamics extends Dynamics {
 
+    /**
+     * 这个评论是最佳的评论
+     */
+    private BugComment bugComment;
+
+    public BugComment getBugComment() {
+        return bugComment;
+    }
+
+    public void setBugComment(BugComment bugComment) {
+        this.bugComment = bugComment;
+    }
 
 
 }
